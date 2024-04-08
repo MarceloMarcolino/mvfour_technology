7f7ce249690a10495d912a1ef8108cce94070551
Marcelo Marcolino <marcelo.marcolino.dev@gmail.com>
2024-04-07 23:51:26 -0300

    feat(sim-proj): implementa a tela de login

---
+++ src/mvfour_technology/Mvfour_technology.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mvfour_technology;
+
+/**
+ *
+ * @author Marcelinho
+ */
+public class Mvfour_technology {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    }
+}
